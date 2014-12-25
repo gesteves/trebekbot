6891f6b550f9799cb9a33f0fb8d0dfdb5b949667
Guillermo Esteves <guillermo.esteves@voxmedia.com>
2014-12-24 20:22:15 -0500

    What are more Trebek responses?

--- app.rb
+++ app.rb
@@ -153,8 +153,8 @@ def get_slack_name(user_id, username)
 end
 
 def trebek_me
-  responses = [ "Welcome back to _Slack Jeopardy_. Before we begin this Jeopardy round, I'd like to ask our contestants once again to please refrain from using ethnic slurs.",
-    "Okay. Turd Ferguson.",
+  responses = [ "Welcome back to Slack Jeopardy. Before we begin this Jeopardy round, I'd like to ask our contestants once again to please refrain from using ethnic slurs.",
+    "Okay, Turd Ferguson.",
     "I hate my job.",
     "That is incorrect.",
     "Let's just get this over with.",
@@ -165,16 +165,18 @@ def trebek_me
     "That is _awful_.",
     "Okay, for the sake of tradition, let's take a look at the answers.",
     "Beautiful. Just beautiful.",
-    "Good for you. Well, as always, three perfectly good charities have been deprived of money, here on Slack Jeopardy. I'm Alex Trebek, and all of you should be ashamed of yourselves! Good night!",
+    "Good for you. Well, as always, three perfectly good charities have been deprived of money, here on Slack Jeopardy. I'm trebekbot, and all of you should be ashamed of yourselves! Good night!",
     "And welcome back to Slack Jeopardy. Because of what just happened before during the commercial, I'd like to apologize to all blind people and children.",
     "Thank you, thank you. Moving on.",
     "I really thought that was going to work.",
-    "Wonderful. Let's take a look at the categories. They are: Potent Potables, Point to your own head, Letters or Numbers, Will this hurt if you put it in your mouth, An album cover, Make any noise, and finally, Famous Muppet Frogs. I should add that the answer to every question in that category is Kermit.",
+    "Wonderful. Let's take a look at the categories. They are: `Potent Potables`, `Point to your own head`, `Letters or Numbers`, `Will this hurt if you put it in your mouth`, `An album cover`, `Make any noise`, and finally, `Famous Muppet Frogs`. I should add that the answer to every question in that category is `Kermit`.",
     "For the last time, that is not a category.",
     "Unbelievable.",
-    "Great. Let's take a look at the final board. And the categories are: Potent Potables; Sharp Things; Movies That Start with the Word Jaws; A Petit Déjeuner - that category is about French phrases, so let's just skip it.",
-    "Enough. Let's just get this over with. Here are the categories, they are: Potent Potables, Countries Between Mexico and Canada, Members of Simon and Garfunkel, I Have a Chardonnay - you choose this category, you automatically get the points and I get to have a glass of wine. Things You Do With a Pencil Sharpener, Tie Your Shoe, and finally, Toast.",
-    "Better luck to all of you, in the next round. It's time for Slack Jeopardy, let's take a look at the board. And the categories are: Potent Potables, Literature - which is just a big word for books - Therapists, Current U.S. Presidents, Show and Tell, Household Objects, and finally, One-Letter Words."]
+    "Great. Let's take a look at the final board. And the categories are: `Potent Potables`, `Sharp Things`, `Movies That Start with the Word Jaws`, `A Petit Déjeuner` - that category is about French phrases, so let's just skip it.",
+    "Enough. Let's just get this over with. Here are the categories, they are: `Potent Potables`, `Countries Between Mexico and Canada`, `Members of Simon and Garfunkel`, `I Have a Chardonnay` - you choose this category, you automatically get the points and I get to have a glass of wine - `Things You Do With a Pencil Sharpener`, `Tie Your Shoe`, and finally, `Toast`.",
+    "Better luck to all of you, in the next round. It's time for Slack Jeopardy, let's take a look at the board. And the categories are: `Potent Potables`, `Literature` - which is just a big word for books - `Therapists`, `Current U.S. Presidents`, `Show and Tell`, `Household Objects`, and finally, `One-Letter Words`.",
+    "Uh, I see. Get back to your podium.",
+    "You look pretty sure of yourself. Think you've got the right answer?"]
     responses.sample
 end
 
