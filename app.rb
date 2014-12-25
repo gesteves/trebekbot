9ff062e0955132ad64d5d80de8bf8a77356f5465
Guillermo Esteves <guillermo.esteves@voxmedia.com>
2014-12-25 12:51:22 -0500

    What is assign value to valueless questions?

--- app.rb
+++ app.rb
@@ -43,6 +43,7 @@ def get_question(params)
   uri = "http://jservice.io/api/random?count=1"
   request = HTTParty.get(uri)
   response = JSON.parse(request.body).first
+  response["value"] = 100 if response["value"].nil?
   question = "The category is `#{response["category"]["title"]}` for $#{response["value"]}: `#{response["question"]}`"
   key = "current_question:#{params[:channel_id]}"
   $redis.set(key, response.to_json)
