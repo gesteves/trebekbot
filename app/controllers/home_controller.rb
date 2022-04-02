67dbb762a26477e5d8fda0b787c511de5551f955
Guillermo Esteves <g@gesteves.com>
2022-04-02 10:27:00 -0600

    Add home page

---
+++ app/controllers/home_controller.rb
@@ -0,0 +1,7 @@
+class HomeController < ApplicationController
+  def index
+  end
+
+  def success
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+  get  "/success"            => "home#success",       :as => "success"
 
   # Defines the root path route ("/")
-  # root "articles#index"
+  root "home#index"
 end
