a90561d20d5507b5ec90007053f9138e30ce8895
Guillermo Esteves <g@gesteves.com>
2022-04-02 10:27:30 -0600

    Add /

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,7 @@ Rails.application.routes.draw do
   # Slack endpoints
   get  "/slack/auth"         => "slack#auth",         :as => "auth"
   post "/slack/interactions" => "slack#interactions", :as => "interactions"
-  post "slack/events"        => "slack#events",       :as => "events"
+  post "/slack/events"        => "slack#events",       :as => "events"
   # Defines the root path route ("/")
   root "home#index"
 end
