f9f0785dddc3cc96d1797eebdb0d24084c9ddda2
Guillermo Esteves <g@gesteves.com>
2022-04-02 15:31:00 -0600

    Remove ephemeral messages

--- app/workers/process_answer_worker.rb
+++ app/workers/process_answer_worker.rb
@@ -4,21 +4,17 @@ class ProcessAnswerWorker < ApplicationWorker
 
     team = Team.find_by(slack_id: team_id)
     game = team.games.find_by(channel: channel_id, ts: ts)
-    if game.has_correct_answer?
-      game.send_ephemeral_message(text: "Sorry, someone already answered!", url: response_url)
-      return
-    end
-    user = User.find_or_create_by(team_id: team.id, slack_id: user_id)
+
     logger.info "Received answer “#{user_answer}“ for game #{game.id} from user #{user.slack_id} in channel #{channel_id} in team #{team_id}"
 
-    answer = Answer.find_by(game: game, user: user)
+    return if game.has_correct_answer?
 
-    if answer.present?
-      game.send_ephemeral_message(text: "You’ve already submitted an answer!", url: response_url)
-    else
-      answer = Answer.new(game: game, user: user, answer: user_answer)
-      answer.save!
-      UpdateGameMessageWorker.perform_async(game.id, response_url)
-    end
+    user = User.find_or_create_by(team_id: team.id, slack_id: user_id)
+    answer = Answer.find_by(game: game, user: user)
+    return if answer.present?
+
+    answer = Answer.new(game: game, user: user, answer: user_answer)
+    answer.save!
+    UpdateGameMessageWorker.perform_async(game.id, response_url)
   end
 end
