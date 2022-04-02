9c25e14251f3033b95258327ac0a20c36e96378c
Guillermo Esteves <g@gesteves.com>
2022-04-02 15:35:00 -0600

    Fix logging

--- app/workers/start_game_worker.rb
+++ app/workers/start_game_worker.rb
@@ -13,7 +13,7 @@ class StartGameWorker < ApplicationWorker
                     channel: channel_id,
                     team: team)
     game.save!
-    logger.info "Starting new game in channel #{channel_id} in team #{team_id}: #{category} for $#{value} “#{question}” (#{answer})"
+    logger.info "Starting new game in channel #{channel_id} in team #{team_id}: #{question.dig(:category, :title)} for $#{question[:value]} “#{question[:question]}” (#{question[:answer]})"
     PostGameMessageWorker.perform_async(game.id)
   end
 end
