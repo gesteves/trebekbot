18c5117ebe9fe5a81e84d89a01a021a28ceebd23
Guillermo Esteves <g@gesteves.com>
2022-04-03 15:14:00 -0600

    Remove air date

--- app/models/game.rb
+++ app/models/game.rb
@@ -43,7 +43,7 @@ class Game < ApplicationRecord
 			elements: [
 				{
 					type: "mrkdwn",
-					text: "*#{category.titleize}* | $#{value} | Aired #{air_date.strftime('%B %-d, %Y')}"
+					text: "*#{category.titleize}* | $#{value}"
 				}
 			]
 		}
