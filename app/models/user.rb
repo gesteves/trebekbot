808b30e5edf06371ffd11df00cb8616b53b1f7c8
Guillermo Esteves <g@gesteves.com>
2022-04-02 20:16:57 -0600

    Mention users correctly

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,24 +6,34 @@ class User < ApplicationRecord
   validates :slack_id, presence: true
 
   def avatar
-    slack = Slack.new
-    response = slack.user_info(access_token: team.access_token, user_id: slack_id)
-    raise response[:error] unless response[:ok]
-    response.dig(:user, :profile, :image_192).presence || response.dig(:user, :profile, :image_72).presence || response.dig(:user, :profile, :image_48).presence || response.dig(:user, :profile, :image_32).presence || response.dig(:user, :profile, :image_24).presence || response.dig(:user, :profile, :image_original).presence
+    Rails.cache.fetch("slack/user/avatar/#{slack_id}", expires_in: 1.day) do
+      slack = Slack.new
+      response = slack.user_info(access_token: team.access_token, user_id: slack_id)
+      raise response[:error] unless response[:ok]
+      response.dig(:user, :profile, :image_192).presence || response.dig(:user, :profile, :image_72).presence || response.dig(:user, :profile, :image_48).presence || response.dig(:user, :profile, :image_32).presence || response.dig(:user, :profile, :image_24).presence || response.dig(:user, :profile, :image_original).presence
+    end
   end
 
   def name
-    slack = Slack.new
-    response = slack.user_info(access_token: team.access_token, user_id: slack_id)
-    raise response[:error] unless response[:ok]
-    response.dig(:user, :real_name).presence || response.dig(:user, :name).presence
+    Rails.cache.fetch("slack/user/name/#{slack_id}", expires_in: 1.day) do
+      slack = Slack.new
+      response = slack.user_info(access_token: team.access_token, user_id: slack_id)
+      raise response[:error] unless response[:ok]
+      response.dig(:user, :real_name).presence || response.dig(:user, :name).presence
+    end
   end
 
   def username
-    slack = Slack.new
-    response = slack.user_info(access_token: team.access_token, user_id: slack_id)
-    raise response[:error] unless response[:ok]
-    response.dig(:user, :name).presence
+    Rails.cache.fetch("slack/user/username/#{slack_id}", expires_in: 1.day) do
+      slack = Slack.new
+      response = slack.user_info(access_token: team.access_token, user_id: slack_id)
+      raise response[:error] unless response[:ok]
+      response.dig(:user, :name).presence
+    end
+  end
+
+  def mention
+    "<@#{slack_id}>"
   end
 
   def add_score(amount)
