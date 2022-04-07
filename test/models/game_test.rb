361af7a61b3934a0aa9ed6ee14c10a5fad930c9d
Guillermo Esteves <g@gesteves.com>
2022-04-07 12:49:16 -0600

    Add quick test

--- test/models/game_test.rb
+++ test/models/game_test.rb
@@ -1,7 +1,11 @@
 require "test_helper"
 
 class GameTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "renders Slack blocks" do
+    game = games(:cremebrulee)
+    blocks = game.to_blocks
+
+    assert_equal "*Desserts & Stuff* | $100 | Aired April 2, 2022", blocks[0][:elements][0][:text]
+    assert_equal "It's the 2-word French name for a custard dessert with a hard, caramelized sugar topping", blocks[1][:label][:text]
+  end
 end
