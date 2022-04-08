0279891011ed866cca5c8f80a5f00513932ac274
Guillermo Esteves <g@gesteves.com>
2022-04-08 08:37:24 -0600

    Add tests

--- test/models/game_test.rb
+++ test/models/game_test.rb
@@ -8,4 +8,22 @@ class GameTest < ActiveSupport::TestCase
     assert_equal "*Desserts & Stuff* | $100 | Aired April 2, 2022", blocks[0][:elements][0][:text]
     assert_equal "It's the 2-word French name for a custard dessert with a hard, caramelized sugar topping", blocks[1][:label][:text]
   end
+
+  test "returns if it has answer by a given user" do
+    game = games(:one)
+    user = users(:three)
+
+    assert_not game.has_answer_by_user?(user)
+
+    answer = Answer.new(answer: "Who is #{game.answer}?", game: game, user: user)
+    answer.save!
+
+    assert game.has_answer_by_user?(user)
+  end
+
+  test "returns array of accepted answers" do
+    game = games(:or)
+
+    assert_equal ['jerry seinfeld or cosmo kramer', 'jerry seinfeld', 'cosmo kramer'], game.accepted_answers
+  end
 end
