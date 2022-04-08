f871496c12499573cd8d1604c5f4f06cde2907f7
Guillermo Esteves <g@gesteves.com>
2022-04-08 08:29:00 -0600

    Fix logging

--- app/models/answer.rb
+++ app/models/answer.rb
@@ -72,7 +72,7 @@ class Answer < ApplicationRecord
     end
     UpdateGameMessageWorker.perform_async(game.id)
     PostMessageWorker.perform_async(message, game.team.slack_id, game.channel, game.ts)
-    logger.info "[LOG] [Game #{game.team.slack_id}] [Channel #{game.channel}] [Game #{game.id}] User answer: #{answer} | Game answer: #{game.answer} | Score: #{similarity_score}"
+    logger.info "[LOG] [Team #{game.team.slack_id}] [Channel #{game.channel}] [Game #{game.id}] User answer: #{answer} | Game answer: #{game.answer} | Score: #{similarity_score}"
   end
 
   def track_mixpanel
