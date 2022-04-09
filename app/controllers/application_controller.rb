f6af2995a970d7023ad90cbe98979d747dc39f90
Guillermo Esteves <g@gesteves.com>
2022-04-09 09:52:56 -0600

    Allow other host

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -14,7 +14,7 @@ class ApplicationController < ActionController::Base
     # Prevent people from bypassing CloudFront and hitting Heroku directly.
     if Rails.env.production? && !is_cloudfront?
       protocol = Rails.configuration.force_ssl ? 'https' : 'http'
-      redirect_to "#{protocol}://#{Rails.application.routes.default_url_options[:host]}#{request.fullpath}", status: 301
+      redirect_to "#{protocol}://#{Rails.application.routes.default_url_options[:host]}#{request.fullpath}", status: 301, allow_other_host: true
     end
   end
 
