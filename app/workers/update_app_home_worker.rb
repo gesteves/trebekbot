8d5d7b779767fd2bacf578cc16e0e59aeaf4f3a2
Guillermo Esteves <g@gesteves.com>
2022-04-10 11:31:22 -0600

    Typo

--- app/workers/update_app_home_worker.rb
+++ app/workers/update_app_home_worker.rb
@@ -3,7 +3,7 @@ class UpdateAppHomeWorker < ApplicationWorker
     return if team_id.blank? || user_id.blank?
     team = Team.find_by(slack_id: team_id)
     return if team.blank?
-    user = Team.users.find_by(slack_id: user_id)
+    user = team.users.find_by(slack_id: user_id)
     return if user.blank?
 
     user.update_app_home
