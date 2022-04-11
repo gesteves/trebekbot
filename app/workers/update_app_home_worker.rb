ad765a8ae2f0fd104a68f3890f59ca19e861fee2
Guillermo Esteves <g@gesteves.com>
2022-04-11 16:21:58 -0600

    Handle new users

--- app/workers/update_app_home_worker.rb
+++ app/workers/update_app_home_worker.rb
@@ -3,9 +3,7 @@ class UpdateAppHomeWorker < ApplicationWorker
     return if team_id.blank? || user_id.blank?
     team = Team.find_by(slack_id: team_id)
     return if team.blank?
-    user = team.users.find_by(slack_id: user_id)
-    return if user.blank?
-
+    user = User.find_or_create_by(team_id: team.id, slack_id: user_id)
     user.update_app_home
   end
 end
