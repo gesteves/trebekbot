c5870a96792f6b43b7e959948f2b7316223ff431
Guillermo Esteves <g@gesteves.com>
2022-04-12 15:44:50 -0600

    Add stats

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -2,6 +2,9 @@ class HomeController < ApplicationController
   before_action :set_max_age
 
   def index
+    @teams_count = Team.all.count
+    @players_count = User.all.count
+    @games_count = Game.all.count
     @noindex = false
   end
 
