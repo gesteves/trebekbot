37321c9932cc4190dbb32950b7e461f2aa006bcf
Guillermo Esteves <g@gesteves.com>
2022-04-13 11:55:10 -0600

    Don't parse response

--- app/lib/slack.rb
+++ app/lib/slack.rb
@@ -202,6 +202,6 @@ class Slack
     response = HTTParty.post("https://slack.com/api/views.publish",
                             body: params.to_json,
                             headers: { 'Authorization': "Bearer #{access_token}", 'Content-Type': 'application/json' })
-    JSON.parse(response.body, symbolize_names: true)
+    response.body
   end
 end
