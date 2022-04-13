0d94ad6c1df0938d5c93f2b8c3ca7a1497ade802
Guillermo Esteves <g@gesteves.com>
2022-04-13 10:15:59 -0600

    Revert

--- app/lib/wikipedia.rb
+++ app/lib/wikipedia.rb
@@ -8,8 +8,6 @@ module Wikipedia
     }
     body = HTTParty.get("https://en.wikipedia.org/w/api.php", query: query).body
     response = JSON.parse(body, symbolize_names: true)
-    index = response[1].index { |e| e =~ regex }
-    return if index.blank?
-    response.last[index]
+    response&.last&.first
   end
 end
