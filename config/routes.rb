3b6b641491d9cb1e44f21023cae05729763a0570
Guillermo Esteves <g@gesteves.com>
2022-09-07 12:58:26 -0600

    Remove public facing pages

--- config/routes.rb
+++ config/routes.rb
@@ -5,14 +5,8 @@ Rails.application.routes.draw do
     ActiveSupport::SecurityUtils.secure_compare(::Digest::SHA256.hexdigest(password), ::Digest::SHA256.hexdigest(ENV["SIDEKIQ_PASSWORD"]))
   end if Rails.env.production?
   mount Sidekiq::Web, at: "/sidekiq"
-
-  get  "/success"            => "home#success",       :as => "success"
-  get  "/privacy"            => "home#privacy",       :as => "privacy"
-
   # Slack endpoints
   get  "/slack/auth"         => "slack#auth",         :as => "auth"
   post "/slack/interactions" => "slack#interactions", :as => "interactions"
   post "/slack/events"       => "slack#events",       :as => "events"
-  # Defines the root path route ("/")
-  root "home#index"
 end
