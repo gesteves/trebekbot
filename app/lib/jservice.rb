7979c99a6cb1bdbb764a1a9de863e04462f4c744
Guillermo Esteves <g@gesteves.com>
2023-12-12 08:44:50 -0700

    Rescue

--- app/lib/jservice.rb
+++ app/lib/jservice.rb
@@ -7,19 +7,23 @@ module Jservice
 
   def self.get_question
     uri = "http://jservice.io/api/random?count=1"
-    request = HTTParty.get(uri)
-    return if response.code != 200
-    response = JSON.parse(request.body, symbolize_names: true).first
-    question = response[:question]
+    begin
+      request = HTTParty.get(uri)
+      return if response.code != 200
+      response = JSON.parse(request.body, symbolize_names: true).first
+      question = response[:question]
 
-    if QUESTION_BLOCKLIST.any? { |phrase| question.downcase.include?(phrase) } || response[:invalid_count].to_i > 0
-      response = get_question
+      if QUESTION_BLOCKLIST.any? { |phrase| question.downcase.include?(phrase) } || response[:invalid_count].to_i > 0
+        response = get_question
+      end
+
+      response[:value] = 200 if response[:value].blank?
+      response[:question] = Sanitize.fragment(response[:question].gsub(/\\/, ""))
+      response[:category][:title] = Sanitize.fragment(response[:category][:title].gsub(/\\/, ""))
+      response[:answer] = Sanitize.fragment(response[:answer].gsub(/\\/, ""))
+      response
+    rescue SocketError
+      nil
     end
-
-    response[:value] = 200 if response[:value].blank?
-    response[:question] = Sanitize.fragment(response[:question].gsub(/\\/, ""))
-    response[:category][:title] = Sanitize.fragment(response[:category][:title].gsub(/\\/, ""))
-    response[:answer] = Sanitize.fragment(response[:answer].gsub(/\\/, ""))
-    response
   end
 end
