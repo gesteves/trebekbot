cf30faf9ee18878825f39f6ab90fd8438a223b5e
Guillermo Esteves <g@gesteves.com>
2023-12-12 08:42:11 -0700

    Auth

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
   Sidekiq::Web.use Rack::Auth::Basic do |username, password|
     ActiveSupport::SecurityUtils.secure_compare(::Digest::SHA256.hexdigest(username), ::Digest::SHA256.hexdigest(ENV["SIDEKIQ_USERNAME"])) &
     ActiveSupport::SecurityUtils.secure_compare(::Digest::SHA256.hexdigest(password), ::Digest::SHA256.hexdigest(ENV["SIDEKIQ_PASSWORD"]))
-  end if Rails.env.production?
+  end if ENV["SIDEKIQ_USE_BASIC_AUTH"].present?
   mount Sidekiq::Web, at: "/sidekiq"
   # Slack endpoints
   get  "/slack/auth"         => "slack#auth",         :as => "auth"
